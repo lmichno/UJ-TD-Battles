46b37b154a1c037ccc465283e6f2f885cd475874
RotBorne <2005brenmat@gmail.com>
2026-01-17 18:15:45 +0100

    feat: dodanie generatora, mhm

--- project/main.cpp
+++ project/main.cpp
@@ -16,13 +16,15 @@ std::mt19937 rng(std::random_device{}());
 float randFloat(float a, float b);
 int randInt(int a, int b);
 
+
 int main() 
 {
     sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "UJ TD 1");
-
+    int money2 = 0;
+    int moneyPerSec = 0;
     TVMENU(window);
 
-    while (window.isOpen()) 
+    while (window.isOpen())
     {
         //MENU 
         MenuResult result = showMenu(window);
@@ -38,9 +40,18 @@ int main()
 
         std::vector<std::unique_ptr<Shooter>> shooters;
         std::vector<std::unique_ptr<Enemy>> enemies;
-
+        int money2 = result.money2;
+        int moneyPerSec = result.moneyPerSec;
         int money = 0;
         int moneyAdd = 5;
+        if (money2 != 0) {
+             money = money2;
+        }
+        if (moneyPerSec != 0) {
+            moneyAdd = moneyPerSec;
+        }
+    
+
         float moneyTimer = 0.0f;
 
         const float gameOverLineX = 190.f;
--- project/menu.hpp
+++ project/menu.hpp
@@ -1,9 +1,23 @@
-#pragma once
+﻿#pragma once
 #include <SFML/Graphics.hpp>
 
-struct MenuResult {
+struct MenuResult
+{
     int lvl;
     int difficulty;
-};
 
+    int money2;
+    int moneyPerSec;
+    float genDifficulty;
+
+    bool fromGenerator; // ← TO JEST TO, CZEGO BRAKOWAŁO
+};
+struct GeneratorResult{
+    int background;
+    int money2;
+    int moneyPerSec;
+    float difficulty;
+};
 MenuResult showMenu(sf::RenderWindow& window);
+GeneratorResult showGeneratorMenu(sf::RenderWindow& window);
+
